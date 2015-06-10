71dc2921c320ede2ec360b60f716d79f62c0eb8f
Ben Bowden <BenBowdenE@gmail.com>
2015-06-10 13:36:06 -0700

    Added home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+root 'pages#home'
+get "about" => "pages#about" #about is url, can change | then pages#about is controller 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
